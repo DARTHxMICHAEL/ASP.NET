fff1895124fe376224bfcf0860ce41cad3375652
Michał Kulikowski <maniek69000000@gmail.com>
2022-12-01 14:48:21 +0100

    Add files via upload
    zadanie 1 oraz 2

---
+++ recruitment tasks/Zad1/Zad1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    class Program
+    {
+        //pusta zmienna, słowo do odwrócenia
+        static string slowo = String.Empty;
+
+        /// <summary>
+        /// Główna funkcja
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //wprowadzenie danych (slowo)
+            Console.Write("Wprowadź słowo do odwrócenia: ");
+            slowo = Console.ReadLine();
+
+            //odpowiedź i wywołanie funkcji [CharInversion]
+            Console.WriteLine("Odwrócone słowo: "+CharInversion(slowo).ToString());
+            Test(); Console.ReadKey(); Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Funkcja do odwracania ciągu znaków
+        /// </summary>
+        /// <param name="tekstDoOdwrocenia">tekst do odwrócenia</param>
+        public static string CharInversion(String tekstDoOdwrocenia)
+        {
+            //inicjalizacja pustych zmiennych
+            string tekstOdwrocony = String.Empty;
+
+            //kopiujemy znaki w odwróconej kolejności
+            for (int i = tekstDoOdwrocenia.Length - 1; i >= 0; i--)
+            {
+                if (tekstDoOdwrocenia[i] >= 'A' && tekstDoOdwrocenia[i] <= 'Z' && i != (tekstDoOdwrocenia.Length - 1))
+                {
+                    //zamieniamy duże litery na małe
+                    tekstOdwrocony += (char)(tekstDoOdwrocenia[i] - 'A' + 'a');
+                }
+                else if(tekstDoOdwrocenia[i] >= 'a' && tekstDoOdwrocenia[i] <= 'z' && i == (tekstDoOdwrocenia.Length - 1))
+                {
+                    //ostatnią-pierwszą literę zamieniamy na dużą
+                    tekstOdwrocony += (char)(tekstDoOdwrocenia[i] - 'a' + 'A');
+                }
+                else
+                {
+                    //resztę zwyczajnie kopiujemy
+                    tekstOdwrocony += tekstDoOdwrocenia[i];
+                }    
+            }
+            return tekstOdwrocony;
+        }
+
+        /// <summary>
+        /// Test [CharInversion]
+        /// </summary>
+        /// <returns></returns>
+        public static void Test()
+        {
+            if (CharInversion("Polonez") == "Zenolop" && CharInversion("POLONEZ") == "Zenolop"
+                && CharInversion("poloneZ") == "Zenolop" && CharInversion("pOlOneZ") == "Zenolop")
+                Console.WriteLine("passed");
+            else
+                Console.WriteLine("failed");
+        }
+
+    }
+}
---
+++ recruitment tasks/Zad2/Zad2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+    class Program
+    {
+        //do testów
+        static string debug;
+
+        static int[,] matrixA = new int[5, 5] 
+          { { 1, 2, 2, 2, 1 }, 
+            { 3, 1, 2, 1, 5 }, 
+            { 3, 3, 1, 5, 5 }, 
+            { 3, 1, 4, 1, 5 }, 
+            { 1, 4, 4, 4, 1 } };
+
+        static int[,] matrixB = new int[4, 4]
+        {
+            { 1, 2, 2, 1 },
+            { 3, 1, 1, 5 },
+            { 3, 1, 1, 5 },
+            { 1, 4, 4, 1 },
+        };
+
+        /// <summary>
+        /// Główna funkcja
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            MatrixCalculate(matrixA);
+            MatrixCalculate(matrixB);
+        }
+
+        /// <summary>
+        /// Kalkulator pól macierzy
+        /// </summary>
+        /// <param name="matrixWejsciowy"> macierz wejściowa </param>
+        public static void MatrixCalculate(int[,] matrixWejsciowy)
+        {
+            //zmienne pomocnicze
+            int k = 1; int j = 1;
+            //pobierz rozmiar macierzy
+            int n = matrixWejsciowy.GetLength(0);
+            //wyznacz srodek macierzy
+            int srodek = matrixWejsciowy.GetLength(0) / 2; 
+
+            //macierz wyjściowa
+            int[,] matrixWyjsciowy = new int[2, 2];
+
+            //pole (górne i lewe)
+            for (int i = 0; i < (srodek); i++)
+            {
+                while(j < (n - k))
+                {
+                    matrixWyjsciowy[0, 0] += matrixWejsciowy[i,j]; //góra
+                    //debug += matrixWejsciowy[i, j].ToString();
+                    matrixWyjsciowy[0, 1] += matrixWejsciowy[j, i]; //lewo
+                    //debug += matrixWejsciowy[j, i].ToString();
+                    j++; 
+                }
+                k++; j = srodek;
+            }
+
+            k = 1; j = 1;
+            //pole (dolne i prawe)
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (j < (n - k))
+                {
+                    matrixWyjsciowy[1, 0] += matrixWejsciowy[i, j]; //dół
+                    //debug += matrixWejsciowy[i, j].ToString();
+                    matrixWyjsciowy[1, 1] += matrixWejsciowy[j, i]; //prawo
+                    //debug += matrixWejsciowy[j, i].ToString();
+                    j++;
+                }
+                k++; j = srodek;
+            }
+
+            //wyświetlenie rezultatów
+            Console.WriteLine(debug+ "\n\n"+"Rezultat:");
+            Console.WriteLine(matrixWyjsciowy[0, 0]+ ", " + matrixWyjsciowy[0, 1]);
+            Console.WriteLine(matrixWyjsciowy[1, 0]+ ", " + matrixWyjsciowy[1, 1]);
+        }
+
+
+    }
+}
